5e5f7f814891a694b3c6a6afec183914561d2f07
Abdul <elizadeh9797@gmail.com>
2023-04-12 23:57:21 +0400

    son durumun hazir version

--- src/test/java/Pages/DialogContent.java
+++ src/test/java/Pages/DialogContent.java
@@ -30,27 +30,6 @@ public class DialogContent extends Parent {
     @FindBy(xpath = "//h6[@class='oxd-text oxd-text--h6 oxd-topbar-header-breadcrumb-module']")
     private WebElement loginSuccess;
 
-//    @FindBy(xpath = "//div[@class='oxd-select-wrapper']")//
-//    private WebElement userRole;
-//
-//    @FindBy(xpath = "(//div[@class='oxd-select-wrapper'])[2]")//
-//    private WebElement status;
-//
-//    @FindBy(xpath = "//div[@class='oxd-autocomplete-wrapper']")//
-//    private WebElement employeeName;
-//
-//    @FindBy(xpath = "(//input[@class='oxd-input oxd-input--active'])[2]")//
-//    private WebElement userNameInput;
-//
-//    @FindBy(xpath = "//input[@type='password']")//
-//    private WebElement passwordInput;
-//
-//    @FindBy(xpath = "(//input[@type='password'])[2]")//
-//    private WebElement confirmPasswordInput;
-//
-//    @FindBy(xpath = "//button[text()=' Cancel ']")//
-//    private WebElement cancelBtn;
-
     @FindBy(xpath = "//button[text()=' Save ']")//
     private WebElement saveBtn;
 
@@ -76,21 +55,6 @@ public class DialogContent extends Parent {
             case "loginSuccess":
                 return loginSuccess;
             case "userRole":
-//                return userRole;
-//            case "status":
-//                return status;
-//            case "employeeName":
-//                return employeeName;
-//            case "userNameInput":
-//                return userNameInput;
-//            case "passwordInput":
-//                return passwordInput;
-//            case "confirmPasswordInput":
-//                return confirmPasswordInput;
-//            case "saveBtn":
-//                return saveBtn;
-//            case "cancelBtn":
-//                return cancelBtn;
             case "addBtn":
                 return addBtn;
 
